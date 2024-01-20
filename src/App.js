2bf7773f0836ced3ee47b16f6ef201beb2947e02
khalig khalig jafaroc <xaliq.ceferov199653@gmail.com>
2024-01-20 22:54:53 +0000

    reactproject

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function App() {
+// function App() {
 
-  const [a,seta] = useState(10)
-  return (
-    <div className="app">
-      <h1>{a}</h1>
-      <button onClick={() => seta((t)=>t+5)}>+</button>
-      <button onClick={() => seta((t)=>t-5)}>-</button>
-    </div>
-  );
-}
+//   const [a,seta] = useState(10)
+//   return (
+//     <div className="app">
+//       <h1>{a}</h1>
+//       <button onClick={() => seta((t)=>t+5)}>+</button>
+//       <button onClick={() => seta((t)=>t-5)}>-</button>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
+// import { useState } from "react";
+
+// function App() {
+
+//   const [a,seta] = useState("Salam,mən")
+//   return (
+//     <div className="app">
+//       <h1>{a}</h1>
+//       <button onClick={() => seta((t)=>t+' adınız-am')}>0</button>
+//       <button onClick={() => seta((t)=>t+' əlaçıyam')}>1</button>
+//       <button onClick={() => seta((t)=>t+ ' yaxşı oxuyanam')}>2</button>
+//       <button onClick={() => seta((t)=>t+ ' orta oxuyanam')}>3</button>
+//     </div>
+//   );
+// }
+
+// export default App;
